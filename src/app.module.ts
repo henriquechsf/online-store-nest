345072f244c0430e95cdb4eabbfc3c29cfa5e66f
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-05-14 17:42:41 -0300

    get data from database repository

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { ProductsController } from './products.controller';
+import { ProductService } from './models/product.service';
+import { Product } from './models/product.entity';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), TypeOrmModule.forFeature([Product])],
   controllers: [AppController, ProductsController],
-  providers: [],
+  providers: [ProductService],
 })
 export class AppModule {}
