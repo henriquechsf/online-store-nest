ada13679b109084a63e8102b986c3b8516b25a79
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-05-14 13:28:26 -0300

    add about page

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,5 +4,25 @@ import { Controller, Get, Render } from '@nestjs/common';
 export class AppController {
   @Get('/')
   @Render('index')
-  index() {}
+  index() {
+    return {
+      title: 'Home Page - Online Store',
+    };
+  }
+
+  @Get('/about')
+  @Render('about')
+  about() {
+    const viewData = [];
+    viewData['description'] = 'This is a about page...';
+    viewData['author'] = 'Developed by: Carlos Henrique';
+
+    const data1 = 'About us - Online Store';
+
+    return {
+      title: data1,
+      subtitle: 'About Us',
+      viewData: viewData,
+    };
+  }
 }
