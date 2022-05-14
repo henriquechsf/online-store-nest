d56b3db48bedb92806e4805f406d1ec591ce046c
Carlos Henrique de S. Ferreira <ch.ferreir@hotmail.com>
2022-05-14 16:55:31 -0300

    connect to mysql database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { ProductsController } from './products.controller';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController, ProductsController],
   providers: [],
 })
